26ef016dcf5363f1fa9c25feb7224a4332bdb1a0
chenxz <chenxiaozhen@ieforever.com>
2018-03-06 23:29:52 +0800

    commit

---
+++ app/src/main/java/com/cxz/music/enums/PlayModeEnum.java
@@ -0,0 +1,33 @@
+package com.cxz.music.enums;
+
+/**
+ * 播放模式
+ * Created by wcy on 2015/12/26.
+ */
+public enum PlayModeEnum {
+    LOOP(0),
+    SHUFFLE(1),
+    SINGLE(2);
+
+    private int value;
+
+    PlayModeEnum(int value) {
+        this.value = value;
+    }
+
+    public static PlayModeEnum valueOf(int value) {
+        switch (value) {
+            case 1:
+                return SHUFFLE;
+            case 2:
+                return SINGLE;
+            case 0:
+            default:
+                return LOOP;
+        }
+    }
+
+    public int value() {
+        return value;
+    }
+}
