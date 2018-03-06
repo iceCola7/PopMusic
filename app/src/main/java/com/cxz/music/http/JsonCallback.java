85115df8cf9a4ce23745220407b9b1d4115f4509
chenxz <chenxiaozhen@ieforever.com>
2018-03-06 23:35:56 +0800

    commit

---
+++ app/src/main/java/com/cxz/music/http/JsonCallback.java
@@ -0,0 +1,32 @@
+package com.cxz.music.http;
+
+import com.google.gson.Gson;
+import com.zhy.http.okhttp.callback.Callback;
+
+import java.io.IOException;
+
+import okhttp3.Response;
+
+/**
+ * Json封装
+ */
+public abstract class JsonCallback<T> extends Callback<T> {
+    private Class<T> clazz;
+    private Gson gson;
+
+    public JsonCallback(Class<T> clazz) {
+        this.clazz = clazz;
+        gson = new Gson();
+    }
+
+    @Override
+    public T parseNetworkResponse(Response response, int id) throws IOException {
+        try {
+            String jsonString = response.body().string();
+            return gson.fromJson(jsonString, clazz);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
